7403685bd3ec232aee834884749b7f3198e5000f
linhngo <thlinh.ngo@gmail.com>
2020-03-04 17:51:25 +0700

    Add flutter_translate package

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc_architecture/route.dart';
 import 'package:flutter_bloc_architecture/theme.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_translate/localized_app.dart';
 
 import 'di/di.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var localizationDelegate = LocalizedApp.of(context).delegate;
+
     return glolalRepository(
       repository: [
         userRepositoryComponent(),
@@ -18,6 +22,13 @@ class App extends StatelessWidget {
         child: MaterialApp(
           title: "Bloc",
           theme: AppTheme.defaultTheme,
+          localizationsDelegates: [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            localizationDelegate
+          ],
+          supportedLocales: localizationDelegate.supportedLocales,
+          locale: localizationDelegate.currentLocale,
           onGenerateRoute: routes(),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_translate/localization_delegate.dart';
+import 'package:flutter_translate/localized_app.dart';
 
 import 'app.dart';
 
-void main() => runApp(App());
+void main() async {
+  var delegate = await LocalizationDelegate.create(
+      fallbackLocale: 'en', supportedLocales: ['en']);
+
+  runApp(LocalizedApp(delegate, App()));
+}
